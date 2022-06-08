08a56fcbc10a14bcea1f68f30562a87629bc6998
Gaganpreet Singh <VE00YM335@YMI.COM>
2022-06-08 12:24:45 +0530

    Parking Solution As per the Problem Statement

---
+++ src/com/parkingsolution/exceptions/GeneralException.java
@@ -0,0 +1,11 @@
+package com.parkingsolution.exceptions;
+
+public class GeneralException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public GeneralException() {
+		super("INVALID CHOICE!");
+	}
+
+}
---
+++ src/com/parkingsolution/exceptions/ParkingIsFullException.java
@@ -0,0 +1,11 @@
+package com.parkingsolution.exceptions;
+
+public class ParkingIsFullException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+
+	public ParkingIsFullException() {
+		super("Parking is Full");
+	}
+
+}
